de8956f9debd75f0a9852657ef68cdc7704ebba6
JeanetteZhang <zhangjinting98@gmail.com>
2021-11-10 10:58:01 -0500

    Update

--- enumerator_for_random_walk.py
+++ enumerator_for_random_walk.py
@@ -1,5 +1,14 @@
+from linear_genetic_programming._program import Program
+from linear_genetic_programming._two_input_boolean_funcs import TwoInputBooleanFuncs
 class EnumeratorForRandomWalk:
-    FUNCLASS = 4
+    RANDOM_WALK_STEP = 10000
+    TARGET_PHENO = 0
 
 if __name__ == "__main__":
-    print("hello, world")
+    prog = Program()
+    for i in range(10000):
+        prog.makeRandomeProg(4, 2, 2, 4)
+        pheno_cur = TwoInputBooleanFuncs.phenotype(prog)
+        for j in range(EnumeratorForRandomWalk.RANDOM_WALK_STEP):
+            prog_update = TwoInputBooleanFuncs.one_step_mutation(prog)
+
